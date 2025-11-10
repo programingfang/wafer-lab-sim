8eec2a486ea88a42066eb1a834799b6e7598f1eb
programingfang <programingfang@gmail.com>
2025-11-11 00:06:51 +0800

    Update 2025-11-11 00:06:51

--- js/video_list.js
+++ js/video_list.js
@@ -3,84 +3,84 @@ window.WAFER_VIDEOS = [
   {
     n: 1,
     qid: 1,
-    title: "IC 是如何製造的-設計",
+    title: "第一關 IC 是如何製造的-設計",
     file: "ic_make_1.mp4",
     thumb: "image1.png"
   },
   {
     n: 2,
     qid: 2,
-    title: "IC 是如何製造的-製造",
+    title: "第二關 IC 是如何製造的-製造",
     file: "ic_make_2.mp4",
     thumb: "image2.png"
   },
   {
     n: 3,
     qid: 3,
-    title: "IC 是如何製造的-封測",
+    title: "第三關 IC 是如何製造的-封測",
     file: "ic_make_3.mp4",
     thumb: "image3.png"
   },
   {
     n: 4,
     qid: 4,
-    title: "晶圓從哪來",
+    title: "第四關 晶圓從哪來",
     file: "ic_make_4.mp4",
     thumb: "image4.png"
   },
   {
     n: 5,
     qid: 5,
-    title: "微小精密的設計圖",
+    title: "第五關 微小精密的設計圖",
     file: "ic_make_5.mp4",
     thumb: "image5.png"
   },
   {
     n: 6,
     qid: 6,
-    title: "設計圖轉印到晶圓上",
+    title: "第六關 設計圖轉印到晶圓上",
     file: "ic_make_6.mp4",
     thumb: "image6.png"
   },
   {
     n: 7,
     qid: 7,
-    title: "設計圖在晶圓上形成溝槽",
+    title: "第七關 設計圖在晶圓上形成溝槽",
     file: "ic_make_7.mp4",
     thumb: "image7.png"
   },
   {
     n: 8,
     qid: 8,
-    title: "如何製造半導體",
+    title: "第八關 如何製造半導體",
     file: "ic_make_8.mp4",
     thumb: "image8.png"
   },
   {
     n: 9,
     qid: 9,
-    title: "如何形成完整的電路",
+    title: "第九關 如何形成完整的電路",
     file: "ic_make_9.mp4",
     thumb: "image9.png"
   },
   {
     n: 10,
     qid: 10,
-    title: "層層堆疊的電路",
+    title: "第十關 層層堆疊的電路",
     file: "ic_make_10.mp4",
     thumb: "image10.png"
   },
   {
     n: 11,
     qid: 11,
-    title: "電子顯微鏡進行檢測",
+    title: "第十一關 電子顯微鏡進行檢測",
     file: "ic_make_11.mp4",
     thumb: "image11.png"
   },
   {
     n: 12,
     qid: 12,
-    title: "無限可能的小晶片",
+    title: "第十二關 無限可能的小晶片",
     file: "ic_make_12.mp4",
     thumb: "image12.png"
   },
